2008b40aed2638802766c483f693baeb31a6e58a
Todd Buiten <tbuiten@launchcg.com>
2018-07-09 20:51:17 +0000

    Add unit tests to verify LdapStrategy

--- test/routes/root.js
+++ test/routes/root.js
@@ -1,12 +1,33 @@
 const request = require('supertest')
 const assert = require('chai').assert
 const sinon = require('sinon')
+
+const EventEmitter = require('events')
 const passport = require('passport')
+const ldap = require('ldapjs');
 
 describe('Test routes', () => {
   let server
+  let ldapStub
+  let ldapSearch
   before(() => {
     server = require('../../bin/www').server
+
+    ldapStub = sinon.stub(ldap, 'createClient')
+    ldapStub.callsFake((params) => {
+      console.log('here')
+      return {
+        bind: function(dn, password, callback) {
+          callback(null)
+        },
+        unbind: function() {},
+        search: function(searchBase, options, callback) {
+          ldapSearch = new EventEmitter()
+          callback(null, ldapSearch)
+        }
+      }
+    })
+
   })
   after(() => {
     server.close()
@@ -15,6 +36,26 @@ describe('Test routes', () => {
   it('should redirect on / when unauthenticated', done => {
     request(server)
       .get('/')
+      .expect(302)
+      .end((err, res) => {
+        assert(!err)
+        done()
+      })
+  })
+
+  it('should authenticate when requested', done => {
+    setTimeout(() => {
+      ldapSearch.emit('searchEntry', {
+        raw: {
+          dn: 'foobar',
+          userPrincipalName: 'Foo Bar'
+        }
+      })
+      ldapSearch.emit('end')
+    }, 10)
+    request(server)
+      .post('/auth/login')
+      .send('username=foobar&password=mypassword&requestSSO=')
       .expect(302)
       .end((err, res) => {
         assert(!err)
